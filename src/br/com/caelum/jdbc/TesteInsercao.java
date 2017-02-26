b256480252191114a6bf82aa67bb482dc103ac5a
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-26 14:31:12 -0300

    C6M4.5 - Quebrando de propósito

--- src/br/com/caelum/jdbc/TesteInsercao.java
+++ src/br/com/caelum/jdbc/TesteInsercao.java
@@ -1,6 +1,5 @@
 package br.com.caelum.jdbc;
 
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -11,33 +10,38 @@ public class TesteInsercao {
 
 	public static void main(String[] args) throws SQLException {
 		String sql;
-		
+
 		sql = "INSERT INTO PRODUTO (nome, descricao) VALUES(?, ?)";
-		
+
 		Connection connection = Database.getConnection();
 		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
 		adiciona("TV LCS", "32 polegadas", statement);
 		adiciona("Blueray", "Full HDMI", statement);
-		
+
 		statement.close();
 		connection.close();
 	}
 
 	private static void adiciona(String nome, String descricao, PreparedStatement statement) throws SQLException {
+
+		if (nome.equals("Blueray")) {
+			throw new IllegalArgumentException("Problema ocorrido");
+		}
+
 		statement.setString(1, nome);
 		statement.setString(2, descricao);
-		
+
 		boolean resultado = statement.execute();
-		
+
 		ResultSet resultSet = statement.getGeneratedKeys();
-		
+
 		while (resultSet.next()) {
 			System.out.println("id = " + resultSet.getString("id"));
 		}
-		
+
 		System.out.println(resultado);
-		
+
 		resultSet.close();
 	}
 }
