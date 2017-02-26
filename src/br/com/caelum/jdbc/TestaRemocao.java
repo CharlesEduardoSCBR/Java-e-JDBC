81ce7cde078a2c7cc42ad1cdca458abc19c5d733
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-26 11:59:41 -0300

    C6M2_2_4 - Inserindo ...

---
+++ src/br/com/caelum/jdbc/TestaRemocao.java
@@ -0,0 +1,20 @@
+package br.com.caelum.jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+
+	public static void main(String[] args) throws SQLException {
+		Connection connection = Database.getConnection();
+		Statement stm = connection.createStatement();
+		stm.execute("delete from Produto where id > 3");
+		
+		int count = stm.getUpdateCount();
+		System.out.println(count + " linhas atualizadas");
+		
+		stm.close();
+		connection.close();
+	}
+}
