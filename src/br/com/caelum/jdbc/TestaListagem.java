25155b5a34a7fa7493607f6ec9d89f05aff3f90f
Charles Eduardo <charles_eduardo_@hotmail.com>
2017-02-26 11:52:53 -0300

    C6M2_2_3 - Inserindo e removendo dados do banco de dados

--- src/br/com/caelum/jdbc/TestaListagem.java
+++ src/br/com/caelum/jdbc/TestaListagem.java
@@ -11,9 +11,16 @@ public class TestaListagem {
 	public static void main(String[] args) throws SQLException {
 		Connection connection = Database.getConnection();
 		Statement statement = connection.createStatement();
-		boolean resultado = statement.execute("insert into Produto (nome, descricao) values ('Notebook', 'Notebook i5')");
+		boolean resultado = statement.execute("insert into Produto (nome, descricao) values ('Notebook', 'Notebook i5')", Statement.RETURN_GENERATED_KEYS);
 		System.out.println("O resultado foi: " + resultado);
 
+		ResultSet resultSet = statement.getGeneratedKeys();
+		
+		while (resultSet.next()) {
+			String id = resultSet.getString("id");
+			System.out.println(id + " gerado");
+		}
+		
 		statement.close();
 		connection.close();
 	}
